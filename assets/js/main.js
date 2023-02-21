3cc8dca75563db0ad092a5758dc14bf04ad1f443
bilal-firekamp <billa@firekamp.com>
2023-02-22 00:11:58 +0700

    Initial push

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+
+(function($) {
+    'use strict';
+
+    AOS.init({
+        duration: 700,
+        easing: 'ease-in-out'
+    });
+    $("#sticker").sticky({topSpacing:0});
+
+// Page Scroll
+jQuery(document).ready(function ($) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 0
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+var showChar = 1;
+var ellipsestext = "";
+var moretext = "+ Read More";
+var lesstext = "- Read Less";
+$('.more').each(function() {
+   var content = $(this).html();
+
+   if(content.length > showChar) {
+
+       var c = content.substr(0, showChar);
+       var h = content.substr(showChar-1, content.length - showChar);
+
+       var html = c + '<span class="moreelipses">'+ellipsestext+'</span><span class="morecontent"><span>' + h + '</span><a href="" class="morelink">'+moretext+'</a></span>';
+
+       $(this).html(html);
+   }
+
+});
+
+$(".morelink").click(function(){
+   if($(this).hasClass("less")) {
+       $(this).removeClass("less");
+       $(this).html(moretext);
+   } else {
+       $(this).addClass("less");
+       $(this).html(lesstext);
+   }
+   $(this).parent().prev().toggle();
+   $(this).prev().toggle();
+   return false;
+});
+    $('#sticker .navbar-expand-lg').onePageNav({
+        currentClass: 'active',
+        changeHash: true,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        easing: 'swing'
+    });
+
+})(jQuery);
